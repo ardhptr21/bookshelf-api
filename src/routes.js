1dce8a0030d22ab83502e5a73e495d7d6385fbbe
ardhptr21 <ardhiputrapradana21@gmail.com>
2021-11-16 08:51:05 +0700

    setup boilerplate books route and initial value for books data

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const routes = [
+  {
+    // menyimpan buku
+    method: 'POST',
+    path: '/books',
+    handler: (request, h) => {}
+  },
+  {
+    // menampilkan seluruh buku
+    method: 'GET',
+    path: '/books',
+    handler: (request, h) => {}
+  },
+  {
+    // menampilkan detail buku
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: (request, h) => {}
+  },
+  {
+    // mengubah data buku
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: (request, h) => {}
+  },
+  {
+    // menghapus data buku
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: (request, h) => {}
+  }
+]
+
+module.exports = routes
